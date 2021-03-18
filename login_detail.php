c560ffcd0f59c20681a2182f9561af43fca98bc3
alibaba04 <albaihaqial@gmail.com>
2021-03-18 11:43:50 +0700

    update app name

--- login_detail.php
+++ login_detail.php
@@ -92,7 +92,7 @@ if (isset($_POST[txtUserID])) {
     <footer class="login-footer" style="padding-right: 20px;">
         
         <div class="pull-right hidden-xs" style="color: white;">
-            <b>Akuntansi App</b> v 1.0.1 &nbsp;&nbsp;<strong>Created by : <a href="http://instagram.com/baihaqial" style="color: white;">alibaba</a>.
+            <b>Accounting App</b> v 1.0.1 &nbsp;&nbsp;<strong>Created by : <a href="http://instagram.com/baihaqial" style="color: white;">alibaba</a>.
             </div>
     </footer>
     <!-- /.login-box -->
